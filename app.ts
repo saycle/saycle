319131f2720049d651fb08ace09313128000b01e
Raphael Mueller <raaphii@gmail.com>
2015-11-26 08:24:11 +0100

    Added basic nodejs project

---
+++ app.ts
@@ -0,0 +1,21 @@
+﻿import express = require('express');
+import http = require('http');
+import path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+http.createServer(app).listen(app.get('port'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
+});
