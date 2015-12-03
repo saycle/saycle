8180dfe4ec75ea29a5d275fa59ae0aee6aad2d87
David Flury <david@flury.email>
2015-12-03 08:47:55 +0100

    Custom dropdownmenu

---
+++ public/scripts/custom-bootstrap.js
@@ -0,0 +1,17 @@
+﻿function dropdownToggle(e) {
+    $(e).parent().toggleClass("open");
+    $(".dropdown-toggle-custom").each(function() {
+        if (!$(this).is(e)) {
+            $(this).parent().removeClass("open");
+        }
+    });
+}
+
+$(document).on('click', function (e) {
+    if (!$(".dropdown-menu-custom").is(e.target) 
+            && $(".dropdown-menu-custom").has(e.target).length === 0 
+            && $(".open").has(e.target).length === 0
+    ) {
+        $(".dropdown-toggle-custom").parent().removeClass("open");
+    }
+});
