251a8e7a5d913d383a22e5fe37eabe4d8eff5045
Raphael Mueller <raaphii@gmail.com>
2015-12-04 20:37:33 +0100

    Added mail send placeholder

---
+++ mails/mails.js
@@ -0,0 +1,11 @@
+//var sendgrid = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
+//sendgrid.send({
+//    to: 'example@example.com',
+//    from: 'other@example.com',
+//    subject: 'Hello World',
+//    text: 'My first email through SendGrid.'
+//}, function (err, json) {
+//    if (err) { return console.error(err); }
+//    console.log(json);
+//}); 
+//# sourceMappingURL=mails.js.map
