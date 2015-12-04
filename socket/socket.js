0d7326a86a73cb1259f17f1a2bd1e4cf5bc31b47
Raphael Mueller <raaphii@gmail.com>
2015-12-04 14:01:42 +0100

    Added missing socket files

---
+++ socket/socket.js
@@ -0,0 +1,22 @@
+var io = null;
+var socket = {
+    init: function (xio) {
+        if (io != null)
+            throw 'IO has been initialized before, cant initialize again';
+        io = xio;
+        // Send current time to all connected clients
+        var sendTime = function () {
+            io.emit('storylocked', { id: '' });
+        };
+        // Send current time every 10 secs
+        setInterval(sendTime, 10000);
+        // Emit welcome message on connection
+        io.on('connection', function (socket) {
+            // Use socket to communicate with this particular client only, sending it it's own id
+            socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+            socket.on('i am client', console.log);
+        });
+    }
+};
+module.exports = socket;
+//# sourceMappingURL=socket.js.map
