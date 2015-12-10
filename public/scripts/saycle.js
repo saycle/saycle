47d6cfb44cd0c159ed10a07bf27af3c4ece7a6f7
Raphael Mueller <raaphii@gmail.com>
2015-12-10 15:21:02 +0100

    Changed to text field

--- public/scripts/saycle.js
+++ public/scripts/saycle.js
@@ -81,28 +81,5 @@
             positionClass: 'toast-bottom-right'
         });
     });
-    
-    // Contenteditable binding
-    app.directive('contenteditable', function () {
-        return {
-            require: 'ngModel',
-            link: function (scope, elm, attrs, ctrl) {
-                // view -> model
-                elm.bind('keyup', function () {
-                    scope.$apply(function () {
-                        ctrl.$setViewValue(elm.html());
-                    });
-                });
-                
-                // model -> view
-                ctrl.$render = function () {
-                    elm.html(ctrl.$viewValue);
-                };
-                
-                // load init value from DOM
-                ctrl.$setViewValue(elm.html());
-            }
-        };
-    });
 
 })();
