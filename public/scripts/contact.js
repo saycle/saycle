7b3a4523afb55d6e1a3cc8b5ce4b2ab4497cceb8
David Flury <david@flury.email>
2015-12-11 11:51:31 +0100

    Contact

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -3,8 +3,38 @@
     var app = angular.module('saycle');
     
     
-    app.controller('contactCtrl', function ($scope, $route) {
+    app.controller('contactCtrl', function($scope, $route, $translate) {
         var vm = this;
+        vm.result = {
+            class: 'hidden',
+            message: ''
+        };
+        vm.FormData = null;
+        vm.submitDisabled = false;
+        vm.submitted = false;
+
+        vm.submit = function(form) {
+            vm.submitted = true;
+            vm.submitDisabled = true;
+            if (form.$valid) {
+                $http({
+                    //send mail...
+                }).success(function(result) {
+                    if (result.success) {
+                        vm.result.message = $translate('contact.successMessage');
+                        vm.result.class = 'bg-success';
+                    } else {
+                        vm.submitDisabled = false;
+                        vm.result.message = $translate('contact.errorMessage');
+                        vm.result.class = 'bg-danger';
+                    }
+                });
+            } else {
+                vm.submitDisabled = false;
+                vm.result.message = $translate('contact.invalidInputMessage');
+                vm.result.class = 'bg-danger';
+            }
+        }
     });
 
 })();
